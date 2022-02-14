23038acdd8e076aa5b8d1be3e1a6d633be0f8beb
takeyu1013 <yuto1013@gmail.com>
2022-02-14 23:53:00 +0900

    ♻️

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io;
+
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer)?;
-    match buffer.trim().parse::<u32>() {
+    match buffer.trim().parse::<usize>() {
         Ok(number) => (0..number).for_each(|_| println!("paiza")),
         Err(e) => println!("{}", e),
     }
