dc7bc7263212f4f1e57c55b33b4e19daa33e5bd8
takeyu1013 <yuto1013@gmail.com>
2022-02-19 11:27:03 +0900

    ♻️

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use std::io;
 
-fn function02() -> io::Result<()> {
+pub fn function02() {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    match buffer.trim().parse::<usize>() {
-        Ok(number) => (0..number).for_each(|_| println!("paiza")),
-        Err(e) => println!("{}", e),
+    if let Ok(_) = io::stdin().read_line(&mut buffer) {
+        if let Ok(number) = buffer.trim().parse::<usize>() {
+            (0..number).for_each(|_| println!("paiza"));
+        }
     }
-    Ok(())
 }
 
-fn function03() {
+pub fn function03() {
     let mut buffer = String::new();
     if let Ok(_) = io::stdin().read_line(&mut buffer) {
         if let Ok(number) = buffer.trim().parse::<usize>() {
             let vector = (0..number)
                 .filter_map(|_| {
                     let mut buffer = String::new();
-                    io::stdin().read_line(&mut buffer).unwrap();
-                    buffer.trim().parse::<usize>().ok()
+                    io::stdin()
+                        .read_line(&mut buffer)
+                        .ok()
+                        .map(|_| buffer.trim().parse::<usize>().ok())
                 })
                 .collect::<Vec<_>>();
-            vector.iter().for_each(|number| println!("{}", number));
+            vector.iter().for_each(|option| {
+                if let Some(number) = option {
+                    println!("{}", number);
+                }
+            });
         }
     }
 }
 
-fn main() -> io::Result<()> {
-    // function02().unwrap();
-    function03();
-    Ok(())
+fn main() {
+    function03()
 }
